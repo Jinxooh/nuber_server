f7cd9bc1aba220fceacf1fc20e363df9151fabf7
jinxooh <cjswp122@gmail.com>
2018-08-02 00:17:05 +0900

    graphql server init

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,21 @@
-class App {
+import { GraphQLServer } from 'graphql-yoga';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
 
+class App {
+  public app: GraphQLServer;
+  constructor () {
+    this.app = new GraphQLServer({
+    });
+    this.middlewares();
+  }
+
+  private middlewares = () => {
+    this.app.express.use(cors());
+    this.app.express.use(morgan('dev'));
+    this.app.express.use(helmet());
+  }
 }
 
-export default App;
+export default new App().app;
