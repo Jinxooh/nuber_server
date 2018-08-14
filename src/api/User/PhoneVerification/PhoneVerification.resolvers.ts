113e002722022ba70ef5202a3a503488f18b5a19
황진수-PC <cjswp122@gmail.com>
2018-08-14 15:21:52 +0900

    send verification sms

---
+++ src/api/User/PhoneVerification/PhoneVerification.resolvers.ts
@@ -0,0 +1,41 @@
+import Verification from "../../../entities/Verification";
+import {
+  PhoneVerificationMutationArgs,
+  PhoneVerificationResponse
+} from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+import { sendVerificationSMS } from "../../../utils/sendSMS";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    PhoneVerification: async (
+      _,
+      args: PhoneVerificationMutationArgs
+    ): Promise<PhoneVerificationResponse> => {
+      const { phoneNumber } = args;
+      try {
+        const existingVerificiation = await Verification.findOne({ payload: phoneNumber })
+        if (existingVerificiation) {
+          existingVerificiation.remove();
+        }
+        const newVerification = await Verification.create({
+          payload: phoneNumber,
+          target: 'PHONE',
+        }).save();
+        console.log(newVerification);
+        await sendVerificationSMS(newVerification.payload, newVerification.key);
+        return {
+          ok: true,
+          error: null,
+        };
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+        }
+      }
+    }
+  }
+};
+
+export default resolvers;
