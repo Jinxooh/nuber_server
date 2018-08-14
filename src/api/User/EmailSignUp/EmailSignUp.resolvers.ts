d19b102dcdabe58c1c9a7c4903ed2d630d215001
황진수-PC <cjswp122@gmail.com>
2018-08-14 17:04:49 +0900

    email Sign up complelte

---
+++ src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
@@ -0,0 +1,47 @@
+import User from "../../../entities/User";
+import {
+  EmailSignUpMutationArgs,
+  EmailSignUpResponse,
+} from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    EmailSignUp: async (
+      _,
+      args: EmailSignUpMutationArgs
+    ): Promise<EmailSignUpResponse> => {
+      const { email } = args;
+      try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+          return {
+            ok: false,
+            error: 'you should log in instead',
+            token: null,
+          }
+        } else {
+          const newUser = await User.create({ ...args }).save();
+          return {
+            ok: true,
+            error: null,
+            token: 'Coming soon',
+          }
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null,
+        }
+      }
+      return {
+        ok: true,
+        error: null,
+        token: null,
+      }
+    }
+  }
+};
+
+export default resolvers;
