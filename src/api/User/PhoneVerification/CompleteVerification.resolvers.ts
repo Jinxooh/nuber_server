8676ae83c03ec4f66a56e5ddac029398a4fc0ce3
황진수-PC <cjswp122@gmail.com>
2018-08-14 16:08:14 +0900

    Complete Verification

---
+++ src/api/User/PhoneVerification/CompleteVerification.resolvers.ts
@@ -0,0 +1,67 @@
+import Verification from "../../../entities/Verification";
+import {
+  CompleteVerificationMutationArgs,
+  CompleteVerificationResponse
+} from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+import User from "../../../entities/User";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    CompleteVerification: async (
+      _,
+      args: CompleteVerificationMutationArgs
+    ): Promise<CompleteVerificationResponse> => {
+      const { phoneNumber, key } = args;
+      try {
+        const verification = await Verification.findOne({
+          payload: phoneNumber,
+          key
+        });
+        if (!verification) {
+          return {
+            ok: false,
+            error: 'Verification key not valid',
+            token: null,
+          }
+        } else {
+          verification.verified = true;
+          verification.save();
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null
+        };
+      }
+
+      try {
+        const user = await User.findOne({ phoneNumber });
+        if (user) {
+          user.verifiedPhoneNumber = true;
+          user.save();
+          return {
+            ok: true,
+            error: null,
+            token: 'Coming soon',
+          };
+        } else {
+          return {
+            ok: true,
+            error: null,
+            token: null,
+          }
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null,
+        }
+      }
+    }
+  }
+};
+
+export default resolvers;
