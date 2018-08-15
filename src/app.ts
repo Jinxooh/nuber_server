c859fca3e477ed8ea8912b5fc942e51d1daed94b
jinxooh <cjswp122@gmail.com>
2018-08-15 14:59:47 +0900

    jwt middleware and inject context at graphql express

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,48 @@
-import cors from 'cors';
-import { GraphQLServer } from 'graphql-yoga';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import schema from './schema';
+import cors from "cors";
+import { GraphQLServer } from "graphql-yoga";
+import helmet from "helmet";
+import morgan from "morgan";
+import schema from "./schema";
+import decodeJWT from "./utils/decodeJWT";
+import { NextFunction, Response } from "express";
 
 class App {
   public app: GraphQLServer;
-  constructor () {
+  constructor() {
     this.app = new GraphQLServer({
       schema,
+      context: req => {
+        return {
+          req: req.request,
+        }
+      }
     });
     this.middlewares();
   }
 
   private middlewares = () => {
     this.app.express.use(cors());
-    this.app.express.use(morgan('dev'));
+    this.app.express.use(morgan("dev"));
     this.app.express.use(helmet());
-  }
+    this.app.express.use(this.jwt);
+  };
+
+  private jwt = async (
+    req,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    const token = req.get("X-JWT");
+    if (token) {
+      const user = await decodeJWT(token);
+      if (user) {
+        req.user = user;
+      } else {
+        req.user = undefined;
+      }
+    }
+    next();
+  };
 }
 
 export default new App().app;
